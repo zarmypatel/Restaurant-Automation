a9083fd180777d5ca49b17e59789dd1bb1b18f01
zarmypatel <56577169+zarmypatel@users.noreply.github.com>
2022-09-10 11:06:34 -0500

    Delete Restaurant-Automation directory

--- Restaurant-Automation/RestaurantAutomation/managechef/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManagechefConfig(AppConfig):
-    name = 'managechef'
--- Restaurant-Automation/RestaurantAutomation/manageorder/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManageorderConfig(AppConfig):
-    name = 'manageorder'
